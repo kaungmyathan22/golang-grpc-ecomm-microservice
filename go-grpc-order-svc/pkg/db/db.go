97d223e23c1c29c5a7e94a76e5a82300c15d97a6
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-05-13 00:37:28 +0630

    feat(order): db connection setup

---
+++ go-grpc-order-svc/pkg/db/db.go
@@ -0,0 +1,27 @@
+package db
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/kaungmyathan22/golang-grpc-order-svc/pkg/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type Handler struct {
+	DB *gorm.DB
+}
+
+func Init(url string) Handler {
+	fmt.Println(url)
+	db, err := gorm.Open(postgres.Open(url), &gorm.Config{})
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	db.AutoMigrate(&models.Order{})
+
+	return Handler{db}
+}
